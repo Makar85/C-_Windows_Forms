8fca0e59f3e1430bf5c3f0dec6c8ea7b76c40a4e
Makar85 <Levitin85@yandex.ru>
2022-01-24 22:32:51 +0300

    обновил лабы

--- ITMO.CSCourse.WFAppLab1.4/ParentForm.cs
+++ ITMO.CSCourse.WFAppLab1.4/ParentForm.cs
@@ -16,6 +16,8 @@ namespace ITMO.CSCourse.WFAppLab1._4
         public ParentForm()
         {
             InitializeComponent();
+            //Свойству Text панели spData устанавливается текущая дата
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString());
         }
 
         private void ExitMenuItem_Click(object sender, EventArgs e)
@@ -26,11 +28,13 @@ namespace ITMO.CSCourse.WFAppLab1._4
         private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
         {
             this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+            spWin.Text = "Windows is cascade";
         }
 
         private void WindowTitleMenuItem_Click(object sender, EventArgs e)
         {
             this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+            spWin.Text = "Windows is horisontal";
         }
 
         private void NewMenuItem_Click(object sender, EventArgs e)
@@ -41,6 +45,35 @@ namespace ITMO.CSCourse.WFAppLab1._4
             newChild.Text = newChild.Text + " " + ++openDocuments;
         }
 
-        
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " + ++openDocuments;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    break;
+                case "Title":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+                       
+
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            spWin.Text = "Windows is cascade";
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            spWin.Text = "Windows is horisontal";
+        }
     }
 }
