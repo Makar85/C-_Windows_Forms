f6105d048ed54e3e2fe2793934f82ad04403d047
Makar85 <Levitin85@yandex.ru>
2022-01-26 23:24:29 +0300

    Практическое задание по WPF

---
+++ WPF.LAB1.WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.LAB1.WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnSetName.IsEnabled = false;
+            btnRetName.IsEnabled = false;
+
+        }
+
+        private void btnSetName_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter("username.txt");
+                sw.WriteLine(textBox1.Text);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            btnRetName.IsEnabled = true;
+
+        }
+
+        private void btnRetName_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("username.txt");
+                label1.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btnSetName.IsEnabled = true;
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            myTextBox.Text = DateTime.Now.ToString();
+        }
+    }
+}
